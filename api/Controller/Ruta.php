ce5e46d7bf8f8e99a44df83bf88124ff194f6d7f
BrayanMedina96 <56609321+BrayanMedina96@users.noreply.github.com>
2020-09-14 13:49:32 -0500

    ok

--- api/Controller/Ruta.php
+++ api/Controller/Ruta.php
@@ -25,6 +25,7 @@ class Ruta
            'Usuario_POST' => 'guardar'  ,
            'Usuario_DELETE' => 'eliminar'  ,
            'Usuario_PUT' => 'actualizar'  ,
+           'Usuario_PUTactualizarAPP' => 'actualizarAPP'  ,
            'Usuario_PUTactualizarPassword' => 'actualizarPassword'  ,
            'Usuario_GETseguridad' => 'seguridad',
            'Usuario_GETperfil' => 'perfil',
